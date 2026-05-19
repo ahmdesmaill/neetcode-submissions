38f1c7998b67e5e76d06ec4c4ae8a57e937c02b6
Ahmed Esmail <ahmdesmaill@proton.me>
2026-05-19 10:17:38 +0300

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.js
@@ -0,0 +1,75 @@
+class DynamicArray {
+    /**
+     * @constructor
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.length = 0;
+        this.cap = capacity;
+        this.array = new Array(capacity);
+    }
+
+    /**
+     * @param {number} i
+     * @returns {number}
+     */
+    get(i) {
+        return this.array[i];
+    }
+
+    /**
+     * @param {number} i
+     * @param {number} n
+     * @returns {void}
+     */
+    set(i, n) {
+        this.array[i] = n;
+    }
+
+    /**
+     * @param {number} n
+     * @returns {void}
+     */
+    pushback(n) {
+        if (this.length === this.cap) {
+            this.resize();
+        }
+        this.array[this.length] = n;
+        this.length++;
+    }
+
+    /**
+     * @returns {number}
+     */
+    popback() {
+        const item = this.array[this.length - 1];
+        this.length--;
+        return item;
+    }
+
+    /**
+     * @returns {void}
+     */
+    resize() {
+        this.cap *= 2;
+        let newArray = new Array(this.array.cap);
+        for (let i = 0; i < this.length; i++) {
+            newArray[i] = this.array[i];
+        }
+        this.array = newArray;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getSize() {
+        return this.length;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getCapacity() {
+        return this.cap;
+    }
+}
